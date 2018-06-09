2c9ba7d11d94d0e24b56eaa479cce5f12c31b89d
Edy Segura <edysegura@gmail.com>
2018-06-08 21:21:25 -0300

    Removed semi-colon

--- js/TodoService.js
+++ js/TodoService.js
@@ -1,41 +1,41 @@
-let dbInstance;
-const dbName = 'todoStore';
-const osName = 'todos';
-const version = 1;
+let dbInstance
+const dbName = 'todoStore'
+const osName = 'todos'
+const version = 1
 
 export default class TodoService {
 
   static upgradeDB(db) {
-    console.info('Creating database');
-    db.createObjectStore('todos', { autoIncrement: true });
+    console.info('Creating database')
+    db.createObjectStore('todos', { autoIncrement: true })
   }
 
   static openDB(resolve, reject) {
-    const request = indexedDB.open(dbName, version);
+    const request = indexedDB.open(dbName, version)
 
     request.addEventListener('upgradeneeded', event =>
       this.upgradeDB(event.target.result)
-    );
+    )
 
     request.addEventListener('success', event => {
-      dbInstance = event.target.result;
-      resolve(dbInstance);
-    });
+      dbInstance = event.target.result
+      resolve(dbInstance)
+    })
 
     request.addEventListener('error', event => {
-      console.error(event);
-      reject(event);
-    });
+      console.error(event)
+      reject(event)
+    })
   }
 
   static getDB() {
     return new Promise((resolve, reject) => {
       if (dbInstance) {
-        resolve(dbInstance);
+        resolve(dbInstance)
       } else {
-        this.openDB(resolve, reject);
+        this.openDB(resolve, reject)
       }
-    });
+    })
   }
 
   static addItem(item) {
@@ -44,14 +44,14 @@ export default class TodoService {
         const request = db
           .transaction(osName, 'readwrite')
           .objectStore(osName)
-          .put(item);
+          .put(item)
 
         request.addEventListener('success', event =>
           resolve(event.target.result)
-        );
+        )
 
-        request.addEventListener('error', event => reject(event.target.result));
-      });
-    });
+        request.addEventListener('error', event => reject(event.target.result))
+      })
+    })
   }
 }
