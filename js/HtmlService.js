42ed22916a1aff59bafcab609af175c08983b8af
Edy Segura <edysegura@gmail.com>
2018-06-13 23:20:00 -0300

    Refactoring index.js to start to use HtmlService.js

--- js/HtmlService.js
+++ js/HtmlService.js
@@ -1,5 +1,33 @@
+const form = document.querySelector('form')
+const ul = document.querySelector('ul')
+
 export default class HtmlService {
   static init() {
-    //
+    this.setSubmitionAction()
+  }
+
+  static addToHtmlList(item) {
+    const li = document.createElement('li')
+    li.textContent = item
+    ul.appendChild(li)
+  }
+
+  static cleanInput(input) {
+    input.value = ''
+    input.focus()
+  }
+
+  static setSubmitionAction() {
+    const onSubmit = event => {
+      event.preventDefault()
+      const item = event.target.item
+
+      TodoService.addItem(item.value).then(() => {
+        addItemToHTML(item.value)
+        cleanInput(item)
+      })
+    }
+
+    form.addEventListener('submit', onSubmit)
   }
 }
--- js/index.js
+++ js/index.js
@@ -1,34 +1,25 @@
 import TodoService from './TodoService.js'
+import HtmlService from './HtmlService.js'
 
 const form = document.querySelector('form')
-const ul = document.querySelector('ul')
 
-function addItemToHTML(item) {
-  const li = document.createElement('li')
-  li.textContent = item
-  ul.appendChild(li)
+function cleanInput(input) {
+  input.value = ''
+  input.focus()
 }
 
-function cleanInput(item) {
-  item.value = ''
-  item.focus()
-}
-
-function loadItems() {
-  TodoService
-    .getList()
-    .then(items => items.forEach(addItemToHTML))
-}
-loadItems()
+TodoService
+  .getList()
+  .then(items => items.forEach(HtmlService.addToHtmlList))
 
 const onSubmit = (event) => {
   event.preventDefault()
-  const item = event.target.item
 
+  const item = event.target.item
   TodoService
     .addItem(item.value)
     .then(() => {
-      addItemToHTML(item.value)
+      HtmlService.addToHtmlList(item.value)
       cleanInput(item)
     })
 }
