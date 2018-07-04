eff2d7ea85a0f89ee030e83abdf336ab4f1db9d6
Edy Segura <edysegura@gmail.com>
2018-07-03 23:03:44 -0300

    Added fetch listener to sw.js

--- sw.js
+++ sw.js
@@ -6,3 +6,8 @@ self.addEventListener('activate', event => {
   console.log('[Service Worker] Activating Service Worker ...', event)
   return self.clients.claim()
 })
+
+self.addEventListener('fetch', event => {
+  console.log('[Service Worker] Fetching something ...', event)
+  event.respondWith(fetch(event.request))
+})
