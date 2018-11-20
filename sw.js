47a81d902a107a30a8e46ea9913976e013f70da3
Edy Segura <edysegura@gmail.com>
2018-11-19 23:46:00 -0200

    Implemeting sw.js caching (not working yet)

--- sw.js
+++ sw.js
@@ -1,6 +1,18 @@
 self.addEventListener('install', event => {
   console.info('[Service Worker] Installing Service Worker ...', event)
-  return self.clients.claim();
+  event.waitUntil(
+    caches.open('todo-list').then(cache => {
+      cache.addAll([
+        '/',
+        '/index.html',
+        '/css/style.css',
+        '/src/js/TodoService.js',
+        '/src/js/HTMLService.js',
+        '/src/js/index.js'
+      ])
+    })
+  )
+  return self.clients.claim()
 })
 
 self.addEventListener('activate', event => {
@@ -10,5 +22,8 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   console.info('[Service Worker] Fetching something ...', event)
-  event.respondWith(fetch(event.request))
+  event.respondWith(
+    caches.match(event.request)
+      .then(request => request)
+  )
 })
